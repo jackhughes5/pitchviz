c74fea378da5a7d2c8ca740d56d5eda7e4da0c42
jackhughes5 <135653289+jackhughes5@users.noreply.github.com>
2026-02-12 12:39:40 -0500

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pybaseball import statcast_pitcher, playerid_lookup, statcast
-import pandas as pd
-
-print(playerid_lookup('Skubal', 'Tarik'))
-
-print(statcast_pitcher('2025-03-27', '2025-05-30', 669373))
